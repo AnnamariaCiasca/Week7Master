e2fbae072efea386c01e557d1913adb7a00e5c4f
Annamaria Ciasca <annamaria.ciasca@avanade.com>
2021-09-07 15:13:35 +0200

    Aggiunta EF

---
+++ Week7Master.RepositoryEF/MasterContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Week7Master.Core.Entities;
+
+namespace Week7Master.RepositoryEF
+{
+    public class MasterContext : DbContext
+    {
+        public DbSet<Corso> Corsi { get; set; }
+        public DbSet<Studente> Studenti { get; set; }
+        public DbSet<Lezione> Lezioni { get; set; }
+        public DbSet<Docente> Docenti { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;
+		                                  Database = CorsiMaster;
+                                          Trusted_Connection = True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+
+    }
+}
